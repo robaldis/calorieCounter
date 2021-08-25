9b780e5ed1ec772e08263e278d0cdc8e8aa1509b
robaldis <robertaldis@gmail.com>
2021-08-26 00:33:32 +0100

    feat: adding basic nav and home page

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
 
     app.get('/', function (req, res) {
-        res.send("ok");
+        res.render("home.html");
     });
     app.get('/about', function (req, res) {
         res.send("ok");
